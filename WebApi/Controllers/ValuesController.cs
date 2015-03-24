d8afed48c327c4b121e5431f396fae4e5f6eabc7
mano92 <sidramano89@gmail.com>
2015-03-25 01:07:06 +0500

    market statics added

--- WebApi/Controllers/ValuesController.cs
+++ WebApi/Controllers/ValuesController.cs
@@ -4,15 +4,38 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApi.Helpers;
+using WebApi.Models;
 
 namespace WebApi.Controllers
 {
     public class ValuesController : ApiController
     {
+        private KarachiStockExchangeData data;
+
+        public ValuesController()
+        {
+            data = new KarachiStockExchangeData();
+        }
+
         // GET api/values
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+        }
+
+        public IEnumerable<string[]> GetExchangeStatics()
+        {
+            //var marketStatics = new ExchangeStatics();
+            var htmlCode = string.Empty;
+            using (var client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.UserAgent, "AvoidError");
+                htmlCode = client.DownloadString("http://dps.kse.com.pk/webpages/ts/tsstats.php?mode=1");
+            }
+            var results = data.ReturnMarketData(htmlCode, "marketData");
+            return results;
+            //return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
